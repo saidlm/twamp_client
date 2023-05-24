39b55e8a64d082eaa15f9a811d4332ae0e918c75
Martin Saidl <martin.saidl@tone.cz>
2023-05-24 17:50:44 +0200

    New configuration schema

--- bindir/probe.sh
+++ bindir/probe.sh
@@ -4,37 +4,59 @@
 DATA=${DATA_DIR}
 BIN=${BIN_DIR}
 
-CONFIG_FILE="global_config.json"
+GLOBAL_CONFIG_FILE="global_config.json"
+LOCAL_CONFIG_FILE="local_config.json"
 TARGETS_FILE="targets_list.json"
 
-# Setting probe source name
+## Using lock to avoid to start more processes
+(
+flock -n 200 || echo "Measuring is still in progress, skipping this run."
+
+## Loading global configuration
+if [ -f "$DATA/$GLOBAL_CONFIG_FILE" ]; then
+
+	DESTINATIONS=''
+
+        cat $DATA/$GLOBAL_CONFIG_FILE | jq -r '.Destinations[] | @base64' | while read line ; do
+                eval "$(echo $line | base64 --decode | jq -r '{ DestinationURL, DestinationPassword } | to_entries | .[] | .key + "=" + (.value | @sh)')"
+                DESTINATIONS="$DESTINATIONS $DestinationPassword@$DestinationURL"
+
+        done
+else
+        echo "Global config is missing! Waiting for 5 minutes."
+        exit 1
+fi
+
+## Loading local configuration
+if [ -f "$DATA/$LOCAL_CONFIG_FILE" ]; then
+        eval "$(cat $DATA/$LOCAL_CONFIG_FILE | jq -r '{ SourceName, Description, Tags } | to_entries | .[] | .key + "=" + (.value | @sh)')"
+
+        SOURCE_DESCR=$Description
+        SOURCE_TAGS=$Tags
+fi
+
+## Setting probe source name
 if [ -z "$PROBE_SOURCE" ]; then
         SOURCE=`hostname -f`
+elif [ -z "$SourceName" ]; then
+        SOURCE=$SourceName
 else
         SOURCE=$PROBE_SOURCE
 fi
 
 echo "Probe source nane: $SOURCE"
 
-# Using lock to avoid to start more processes
-(
-flock -n 200 || echo "Measuring is still in progress, skipping this run."
-
-## Loading global configuration
-if [ -f "$DATA/$CONFIG_FILE" ]; then
-	eval "$(cat $DATA/$CONFIG_FILE | jq -r '.Destination | { DestinationURL, DestinationPassword } | to_entries | .[] | .key + "=" + (.value | @sh)')"
-else
-        echo "Global config is missing! Waiting for 5 minutes."
-        exit 1
-fi
 
 ## Main loop
 if [ -f "$DATA/$TARGETS_FILE" ]; then
 	cat $DATA/$TARGETS_FILE | jq -r '.Targets[] | @base64' | while read line ; do
-		eval "$(echo $line | base64 --decode | jq -r '{ IP, DSCP, Custom_id, Latitude, Longitude} | to_entries | .[] | .key + "=" + (.value | @sh)')"
+		eval "$(echo $line | base64 --decode | jq -r '{ IP, DSCP, Description, Latitude, Longitude, Tags} | to_entries | .[] | .key + "=" + (.value | @sh)')"
+
+                TARGET_DESCR=$Description
+                TAGS="$SOURCE_TAGS $Tags"		
 
 		echo -n "Processing $IP ... "
-		$BIN/metis_twmping.sh -t "$IP" -q "$DSCP" -d "$DestinationURL" -a "$DestinationPassword" -s "$SOURCE" -c "$Custom_comment"
+		$BIN/metis_twmping.sh -t "$IP" -q "$DSCP" -d "$DESTINATIONS" -s "$SOURCE" -c "$TARGET_DESCR"
 		ERR=$?
 		if [ $ERR -eq 124 ]; then
 			echo "Destination host is not responding; timeout."
